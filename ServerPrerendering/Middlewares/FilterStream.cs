4f283d0d19f0b417b9adc6d95eef62d3041172a0
jsakamoto <junichi.jp@gmail.com>
2019-09-13 22:24:38 +0900

    Server Pre-rendering - Improve response filtering

---
+++ ServerPrerendering/Middlewares/FilterStream.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Toolbelt.Blazor.HeadElement.Internals;
+
+namespace Toolbelt.Blazor.HeadElement.Middlewares
+{
+    internal delegate Task WriteAsyncInvoker(byte[] buffer, int offset, int count, CancellationToken cancellationToken);
+
+    internal delegate void FlushInvoker();
+
+    internal class FilterStream : Stream
+    {
+        private readonly HttpContext HttpContext;
+
+        private readonly IHeadElementHelperStore InternalStore;
+
+        internal readonly MemoryStream MemoryStream;
+
+        internal readonly Stream OriginalStream;
+
+        private bool _IsCaptured;
+
+        private WriteAsyncInvoker WriteAsyncInvoker;
+
+        private FlushInvoker FlushInvoker;
+
+        public override bool CanRead => false;
+
+        public override bool CanSeek => false;
+
+        public override bool CanTimeout => false;
+
+        public override bool CanWrite => true;
+
+        public override long Length => throw new NotSupportedException();
+
+        public override long Position
+        {
+            get => throw new NotSupportedException();
+            set => throw new NotSupportedException();
+        }
+
+        public FilterStream(HttpContext httpContext, IHeadElementHelperStore internalStore)
+        {
+            this.HttpContext = httpContext;
+            this.InternalStore = internalStore;
+            this.MemoryStream = new MemoryStream();
+            this.OriginalStream = this.HttpContext.Response.Body;
+            this.HttpContext.Response.Body = this;
+
+            this.WriteAsyncInvoker = DefaultWriteAsyncInvoker;
+            this.FlushInvoker = DefaultFlushInvoker;
+        }
+
+        private Task DefaultWriteAsyncInvoker(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            return RebindInvokers().WriteAsync(buffer, offset, count, cancellationToken);
+        }
+
+        private void DefaultFlushInvoker()
+        {
+            RebindInvokers().Flush();
+        }
+
+        private Stream RebindInvokers()
+        {
+            _IsCaptured = (HttpContext.Response.ContentType?.StartsWith("text/html") == true && InternalStore.Title != null);
+            var stream = _IsCaptured ? MemoryStream : OriginalStream;
+            this.FlushInvoker = stream.Flush;
+            this.WriteAsyncInvoker = stream.WriteAsync;
+            return stream;
+        }
+
+        internal bool IsCaptured() => _IsCaptured;
+
+        public override int Read(byte[] buffer, int offset, int count) => throw new NotSupportedException();
+
+        public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
+
+        public override void SetLength(long value) => throw new NotSupportedException();
+
+        public override void Write(byte[] buffer, int offset, int count) => throw new NotSupportedException();
+
+        public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            return WriteAsyncInvoker(buffer, offset, count, cancellationToken);
+        }
+
+        public override void Flush() => FlushInvoker();
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            this.MemoryStream.Dispose();
+            this.OriginalStream.Dispose();
+        }
+    }
+}
--- ServerPrerendering/Middlewares/HeadElementServerPrerenderingMiddleware.cs
+++ ServerPrerendering/Middlewares/HeadElementServerPrerenderingMiddleware.cs
@@ -19,17 +19,15 @@ namespace Toolbelt.Blazor.HeadElement.Middlewares
 
         public async Task InvokeAsync(HttpContext context, IHeadElementHelperStore internalStore)
         {
-            using var memStream = new MemoryStream();
-            var orgStream = context.Response.Body;
-            context.Response.Body = memStream;
+            var filter = new FilterStream(context, internalStore);
 
             await _next(context);
 
-            if (context.Response.ContentType.StartsWith("text/html") && internalStore.Title != null)
+            if (filter.IsCaptured())
             {
-                memStream.Seek(0, SeekOrigin.Begin);
+                filter.MemoryStream.Seek(0, SeekOrigin.Begin);
                 var parser = new HtmlParser();
-                using var doc = parser.ParseDocument(memStream);
+                using var doc = parser.ParseDocument(filter.MemoryStream);
                 internalStore.DefaultTitle = doc.Title ?? "";
                 doc.Title = internalStore.Title;
 
@@ -38,14 +36,14 @@ namespace Toolbelt.Blazor.HeadElement.Middlewares
                 script.SetAttribute("type", "text/default-title");
                 doc.Head.AppendChild(script);
 
-                memStream.SetLength(0);
+                filter.MemoryStream.SetLength(0);
                 var encoding = Encoding.UTF8;
-                using var writer = new StreamWriter(memStream, bufferSize: -1, leaveOpen: true, encoding: encoding) { AutoFlush = true };
+                using var writer = new StreamWriter(filter.MemoryStream, bufferSize: -1, leaveOpen: true, encoding: encoding) { AutoFlush = true };
                 doc.ToHtml(writer, new PrettyMarkupFormatter());
-            }
 
-            memStream.Seek(0, SeekOrigin.Begin);
-            await memStream.CopyToAsync(orgStream);
+                filter.MemoryStream.Seek(0, SeekOrigin.Begin);
+                await filter.MemoryStream.CopyToAsync(filter.OriginalStream);
+            }
         }
     }
 }
