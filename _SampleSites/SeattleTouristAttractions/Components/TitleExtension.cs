bcf1616a84a3cafb9ca58ee2f010a14ed3c8edef
jsakamoto <junichi.jp@gmail.com>
2021-01-15 22:07:14 +0900

    "Seattle Tourist Attractions" - implement a utility function to adding the suffix to the title.

---
+++ _SampleSites/SeattleTouristAttractions/Components/TitleExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace SeattleTouristAttractions.Components
+{
+    public static class TitleExtension
+    {
+        public static string WithSuffix(string title, bool nosuffix = false)
+        {
+            return nosuffix ? title : title + " - Seattle Tourist Attractions";
+        }
+    }
+}
