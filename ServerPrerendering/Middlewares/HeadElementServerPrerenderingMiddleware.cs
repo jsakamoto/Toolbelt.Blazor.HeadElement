2e7eee9b639570b75bed11767b980b48ffd0cff9
jsakamoto <junichi.jp@gmail.com>
2021-05-09 15:16:12 +0900

    Fix: The server-side rendering result lost a slash of a self-closing tag.

--- ServerPrerendering/Middlewares/HeadElementServerPrerenderingMiddleware.cs
+++ ServerPrerendering/Middlewares/HeadElementServerPrerenderingMiddleware.cs
@@ -7,7 +7,6 @@ using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using AngleSharp.Dom;
-using AngleSharp.Html;
 using AngleSharp.Html.Dom;
 using AngleSharp.Html.Parser;
 using Microsoft.AspNetCore.Http;
@@ -52,7 +51,8 @@ namespace Toolbelt.Blazor.HeadElement.Middlewares
                     filter.MemoryStream.SetLength(0);
                     var encoding = Encoding.UTF8;
                     using var writer = new StreamWriter(filter.MemoryStream, bufferSize: -1, leaveOpen: true, encoding: encoding) { AutoFlush = true };
-                    doc.ToHtml(writer, new HtmlMarkupFormatter());
+                    // WORKAROUND: The HtmlMarkupFormatter class in AngleSharp doesn't append slash for closing tag, so I implemented custom version of it.
+                    doc.ToHtml(writer, new CustomHtmlMarkupFormatter());
 
                     filter.MemoryStream.Seek(0, SeekOrigin.Begin);
                     await filter.MemoryStream.CopyToAsync(filter.OriginalStream);
