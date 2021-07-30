3f7a06e5bd17b54ceb422054bb1cc9f4ae63b7f7
jsakamoto <junichi.jp@gmail.com>
2021-07-30 20:38:44 +0900

    Apply code clean up.

--- Services/HeadElementHelperService.cs
+++ Services/HeadElementHelperService.cs
@@ -40,224 +40,224 @@ namespace Toolbelt.Blazor.HeadElement
             this._JS = services.GetService<IJSRuntime>();
             this._NavigationManager = services.GetRequiredService<NavigationManager>();
             this._Store = services.GetRequiredService<IHeadElementHelperStore>();
-            this._NavigationManager.LocationChanged += _NavigationManager_LocationChanged;
+            this._NavigationManager.LocationChanged += this._NavigationManager_LocationChanged;
         }
 
         public void Dispose()
         {
-            _NavigationManager.LocationChanged -= _NavigationManager_LocationChanged;
+            this._NavigationManager.LocationChanged -= this._NavigationManager_LocationChanged;
         }
 
         private async ValueTask<bool> EnsureScriptEnabledAsync()
         {
-            if (_ScriptEnabled || _JS == null) return _ScriptEnabled;
+            if (this._ScriptEnabled || this._JS == null) return this._ScriptEnabled;
 
-            await _EnsureScriptSyncer.WaitAsync();
+            await this._EnsureScriptSyncer.WaitAsync();
             try
             {
-                if (_ScriptEnabled) return _ScriptEnabled;
+                if (this._ScriptEnabled) return this._ScriptEnabled;
 
                 var version = this.GetType().Assembly.GetName().Version;
-                var script = Options.DisableClientScriptAutoInjection ? "0" :
+                var script = this.Options.DisableClientScriptAutoInjection ? "0" :
                     $"new Promise(r=>((d,t,s)=>(h=>h.querySelector(t+`[src=\"${{s}}\"]`)?r():(e=>(e.src=s,e.onload=r,h.appendChild(e)))(d.createElement(t)))(d.head))(document,'script','_content/Toolbelt.Blazor.HeadElement.Services/script.min.js?v={version}'))";
-                await _JS.InvokeVoidAsync("eval", script);
+                await this._JS.InvokeVoidAsync("eval", script);
 
-                _ScriptEnabled = true;
+                this._ScriptEnabled = true;
             }
             catch { }
-            finally { _EnsureScriptSyncer.Release(); }
-            return _ScriptEnabled;
+            finally { this._EnsureScriptSyncer.Release(); }
+            return this._ScriptEnabled;
         }
 
         private async ValueTask<T> InvokeJSAsync<T>(string identifier, params object[] args)
         {
-            if (await EnsureScriptEnabledAsync())
+            if (await this.EnsureScriptEnabledAsync())
             {
-                return await _JS.InvokeAsync<T>(identifier, args);
+                return await this._JS.InvokeAsync<T>(identifier, args);
             }
             return default;
         }
 
         private void _NavigationManager_LocationChanged(object sender, LocationChangedEventArgs e)
         {
-            ResetIfNeededAsync().ConfigureAwait(false);
+            this.ResetIfNeededAsync().ConfigureAwait(false);
         }
 
         private async ValueTask ResetIfNeededAsync()
         {
-            await _ResetSyncer.WaitAsync();
+            await this._ResetSyncer.WaitAsync();
             try
             {
-                await GetDefaultsAsync();
+                await this.GetDefaultsAsync();
 
-                if (_Store.UrlLastSet == NormalizeUrl(_NavigationManager.Uri)) return;
+                if (this._Store.UrlLastSet == this.NormalizeUrl(this._NavigationManager.Uri)) return;
 
-                var urlLastSet = _Store.UrlLastSet;
-                var normalizeUrl = NormalizeUrl(_NavigationManager.Uri);
+                var urlLastSet = this._Store.UrlLastSet;
+                var normalizeUrl = this.NormalizeUrl(this._NavigationManager.Uri);
 
-                _Store.MetaElementCommands.Clear();
-                _Store.LinkElementCommands.Clear();
+                this._Store.MetaElementCommands.Clear();
+                this._Store.LinkElementCommands.Clear();
 
-                if (_Store.DefaultTitle != null)
+                if (this._Store.DefaultTitle != null)
                 {
-                    await ResetTitleAsync();
+                    await this.ResetTitleAsync();
                 }
-                if (_Store.DefaultMetaElements != null)
+                if (this._Store.DefaultMetaElements != null)
                 {
-                    await ResetMetaElementsAsync();
+                    await this.ResetMetaElementsAsync();
                 }
-                if (_Store.DefaultLinkElements != null)
+                if (this._Store.DefaultLinkElements != null)
                 {
-                    await ResetLinkElementsAsync();
+                    await this.ResetLinkElementsAsync();
                 }
 
-                _Store.UrlLastSet = NormalizeUrl(_NavigationManager.Uri);
+                this._Store.UrlLastSet = this.NormalizeUrl(this._NavigationManager.Uri);
             }
-            finally { _ResetSyncer.Release(); }
+            finally { this._ResetSyncer.Release(); }
         }
 
         private async ValueTask GetDefaultsAsync()
         {
-            await GetDefaultTitleAsync();
-            await GetDefaultMetaElementsAsync();
-            await GetDefaultLinkElementsAsync();
+            await this.GetDefaultTitleAsync();
+            await this.GetDefaultMetaElementsAsync();
+            await this.GetDefaultLinkElementsAsync();
         }
 
-        public ValueTask<string> GetTitleAsync() => new(_Store.Title);
+        public ValueTask<string> GetTitleAsync() => new(this._Store.Title);
 
         public async ValueTask SetTitleAsync(string title)
         {
-            await _Syncer.WaitAsync();
+            await this._Syncer.WaitAsync();
             try
             {
-                await ResetIfNeededAsync();
-                _Store.Title = title;
-                await InvokeJSAsync<object>(NST + "set", title);
+                await this.ResetIfNeededAsync();
+                this._Store.Title = title;
+                await this.InvokeJSAsync<object>(NST + "set", title);
             }
-            finally { _Syncer.Release(); }
+            finally { this._Syncer.Release(); }
         }
 
         private async ValueTask ResetTitleAsync()
         {
-            _Store.Title = _Store.DefaultTitle;
-            await InvokeJSAsync<object>(NST + "set", _Store.DefaultTitle);
+            this._Store.Title = this._Store.DefaultTitle;
+            await this.InvokeJSAsync<object>(NST + "set", this._Store.DefaultTitle);
         }
 
         private string NormalizeUrl(string uri)
         {
-            var absoluteUri = _NavigationManager.ToAbsoluteUri(uri);
+            var absoluteUri = this._NavigationManager.ToAbsoluteUri(uri);
             return absoluteUri.AbsolutePath.TrimEnd('/') + absoluteUri.Query;
         }
 
         public ValueTask SetDefaultTitleAsync(string defaultTitle)
         {
-            _Store.DefaultTitle = defaultTitle;
+            this._Store.DefaultTitle = defaultTitle;
             return new ValueTask();
         }
 
         public async ValueTask<string> GetDefaultTitleAsync()
         {
-            if (_Store.DefaultTitle == null)
+            if (this._Store.DefaultTitle == null)
             {
-                _Store.DefaultTitle = await InvokeJSAsync<string>(NST + "query");
+                this._Store.DefaultTitle = await this.InvokeJSAsync<string>(NST + "query");
             }
-            return _Store.DefaultTitle;
+            return this._Store.DefaultTitle;
         }
 
         public async ValueTask<IEnumerable<MetaElement>> GetDefaultMetaElementsAsync()
         {
-            if (_Store.DefaultMetaElements == null)
+            if (this._Store.DefaultMetaElements == null)
             {
-                _Store.DefaultMetaElements = await InvokeJSAsync<MetaElement[]>(NSM + "query");
+                this._Store.DefaultMetaElements = await this.InvokeJSAsync<MetaElement[]>(NSM + "query");
             }
-            return _Store.DefaultMetaElements;
+            return this._Store.DefaultMetaElements;
         }
 
         public async ValueTask SetMetaElementsAsync(params MetaElement[] elements)
         {
-            await _Syncer.WaitAsync();
+            await this._Syncer.WaitAsync();
             try
             {
-                await ResetIfNeededAsync();
-                await InvokeJSAsync<object>(NSM + "set", new object[] { elements });
+                await this.ResetIfNeededAsync();
+                await this.InvokeJSAsync<object>(NSM + "set", new object[] { elements });
 
                 var commands = elements.Select(elem => new MetaElementCommand { Operation = MetaElementOperations.Set, Element = elem });
                 foreach (var command in commands)
                 {
-                    _Store.MetaElementCommands.Add(command);
+                    this._Store.MetaElementCommands.Add(command);
                 }
             }
-            finally { _Syncer.Release(); }
+            finally { this._Syncer.Release(); }
         }
 
         public async ValueTask RemoveMetaElementsAsync(params MetaElement[] elements)
         {
-            await _Syncer.WaitAsync();
+            await this._Syncer.WaitAsync();
             try
             {
-                await ResetIfNeededAsync();
-                await InvokeJSAsync<object>(NSM + "del", new object[] { elements });
+                await this.ResetIfNeededAsync();
+                await this.InvokeJSAsync<object>(NSM + "del", new object[] { elements });
 
                 var commands = elements.Select(elem => new MetaElementCommand { Operation = MetaElementOperations.Remove, Element = elem });
                 foreach (var command in commands)
                 {
-                    _Store.MetaElementCommands.Add(command);
+                    this._Store.MetaElementCommands.Add(command);
                 }
             }
-            finally { _Syncer.Release(); }
+            finally { this._Syncer.Release(); }
         }
 
         private async ValueTask ResetMetaElementsAsync()
         {
-            await InvokeJSAsync<object>(NSM + "reset", _Store.DefaultMetaElements);
+            await this.InvokeJSAsync<object>(NSM + "reset", this._Store.DefaultMetaElements);
         }
 
         public async ValueTask<IEnumerable<LinkElement>> GetDefaultLinkElementsAsync()
         {
-            if (_Store.DefaultLinkElements == null)
+            if (this._Store.DefaultLinkElements == null)
             {
-                _Store.DefaultLinkElements = await InvokeJSAsync<LinkElement[]>(NSL + "query");
+                this._Store.DefaultLinkElements = await this.InvokeJSAsync<LinkElement[]>(NSL + "query");
             }
-            return _Store.DefaultLinkElements;
+            return this._Store.DefaultLinkElements;
         }
 
         public async ValueTask SetLinkElementsAsync(params LinkElement[] elements)
         {
-            await _Syncer.WaitAsync();
+            await this._Syncer.WaitAsync();
             try
             {
-                await ResetIfNeededAsync();
-                await InvokeJSAsync<object>(NSL + "set", new object[] { elements });
+                await this.ResetIfNeededAsync();
+                await this.InvokeJSAsync<object>(NSL + "set", new object[] { elements });
 
                 var commands = elements.Select(elem => new LinkElementCommand { Operation = LinkElementOperations.Set, Element = elem });
                 foreach (var command in commands)
                 {
-                    _Store.LinkElementCommands.Add(command);
+                    this._Store.LinkElementCommands.Add(command);
                 }
             }
-            finally { _Syncer.Release(); }
+            finally { this._Syncer.Release(); }
         }
 
         public async ValueTask RemoveLinkElementsAsync(params LinkElement[] elements)
         {
-            await _Syncer.WaitAsync();
+            await this._Syncer.WaitAsync();
             try
             {
-                await ResetIfNeededAsync();
-                await InvokeJSAsync<object>(NSL + "del", new object[] { elements });
+                await this.ResetIfNeededAsync();
+                await this.InvokeJSAsync<object>(NSL + "del", new object[] { elements });
 
                 var commands = elements.Select(elem => new LinkElementCommand { Operation = LinkElementOperations.Remove, Element = elem });
                 foreach (var command in commands)
                 {
-                    _Store.LinkElementCommands.Add(command);
+                    this._Store.LinkElementCommands.Add(command);
                 }
             }
-            finally { _Syncer.Release(); }
+            finally { this._Syncer.Release(); }
         }
 
         private async ValueTask ResetLinkElementsAsync()
         {
-            await InvokeJSAsync<object>(NSL + "reset", _Store.DefaultLinkElements);
+            await this.InvokeJSAsync<object>(NSL + "reset", this._Store.DefaultLinkElements);
         }
     }
 }
