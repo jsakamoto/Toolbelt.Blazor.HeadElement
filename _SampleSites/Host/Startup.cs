ad6e22241abe4b18e8b3468bce1fce3d8e3673cc
jsakamoto <junichi.jp@gmail.com>
2021-07-31 20:11:56 +0900

    Update sample sites for multi targeting support.

--- _SampleSites/Host/Startup.cs
+++ _SampleSites/Host/Startup.cs
@@ -14,10 +14,7 @@ namespace SampleSite.Host
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHeadElementHelper(options =>
-            {
-                //options.DisableClientScriptAutoInjection = true;
-            });
+            services.AddHeadElementHelper();
             services.AddSingleton<IWeatherForecastService, DummyWeatherForecastService>();
 
             services.AddControllersWithViews();
