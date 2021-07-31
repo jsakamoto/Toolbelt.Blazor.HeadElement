015d35b8f89f19b99dc6626b81740443f4756f1e
jsakamoto <junichi.jp@gmail.com>
2021-07-31 20:05:25 +0900

    Deprecate "HeadElementHelperServiceOptions"

--- Services/HeadElementDependencyInjection.cs
+++ Services/HeadElementDependencyInjection.cs
@@ -9,9 +9,15 @@ namespace Toolbelt.Blazor.Extensions.DependencyInjection
     {
         public static IServiceCollection AddHeadElementHelper(this IServiceCollection services)
         {
+#pragma warning disable CS0618 // Type or member is obsolete
             return services.AddHeadElementHelper(configure: null);
+#pragma warning restore CS0618 // Type or member is obsolete
         }
 
+#if ENABLE_JSMODULE
+        [System.Obsolete("The \"DisableClientScriptAutoInjection\" option is no longer effective in .net 5.0.")]
+        [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+#endif
         public static IServiceCollection AddHeadElementHelper(this IServiceCollection services, Action<HeadElementHelperServiceOptions> configure)
         {
             services.AddScoped<IHeadElementHelperStore, HeadElementHelperStore>();
